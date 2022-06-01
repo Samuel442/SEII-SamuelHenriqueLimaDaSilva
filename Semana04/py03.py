f550d80a9bab0deb682ebd6ae27647744f2fd6dd
Samuel442_samuelhenrique442@gmail.com <samuelhenrique442@gmail.com>
2022-06-01 09:26:31 -0300

    Até o momento terminei até o arquivo py03.py

--- Semana04/py03.py
+++ Semana04/py03.py
@@ -1,2 +1,43 @@
+#Aula 3 video
+
+#Oparadores aritméticos
+
+#Adição               3+2
+#Subtração            3-2
+#Mutiplicação         3*2
+#Divisão              3/2
+#Floor divisão        3//2
+#Exponencial          3 ** 2
+#Módulo               3%2
+
+#Comparações
+
+#Igual               3 == 2
+#Não igual           3!= 2
+#Maior que           3 > 2
+#Menor que           3 < 2
+#Maior pou igual     3 >= 2
+#Menor ou igual      3 <= 2
+
 num = 3
 
+num_1 = 100
+num_2  =200
+
+print (2 % 2)
+print (3 % 2)
+print (4 % 2)
+print (5 % 2)
+
+print ((3 + 1) +  2)
+
+print (abs(-3))
+print (round(3.75,1))
+
+
+print (num_1 == num_2)
+print (num_1 != num_2)
+print (num_1 > num_2)
+print (num_1 < num_2)
+print (num_1 >= num_2)
+print (num_1 <= num_2)
