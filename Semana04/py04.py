147dfd1609115db27439bb5d381fa00269ef0aa6
Samuel442_samuelhenrique442@gmail.com <samuelhenrique442@gmail.com>
2022-06-01 19:10:37 -0300

    Terminei o arquivo 4

--- Semana04/py04.py
+++ Semana04/py04.py
@@ -1,1 +1,21 @@
 #Aula 04
+
+list_1 = ['History','Math','Phisics','CompSci']
+list_2 = list_1
+
+print (list_1)
+print (list_2)
+
+#list_1[10] = 'Art'
+
+#print (list_1)
+#print (list_2)
+
+#Immutable
+
+#tuple_1 = ('History','Math','Phisics','CompSci')
+#tuple_2 =tuple_1
+
+#print (tuple_1)
+#print (tuple_2)
+
