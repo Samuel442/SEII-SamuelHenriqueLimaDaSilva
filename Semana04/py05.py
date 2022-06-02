94398aca60d096aa6437d4e0c82b84dfc694d62b
Samuel442_samuelhenrique442@gmail.com <samuelhenrique442@gmail.com>
2022-06-02 19:08:03 -0300

    Terminado o arquivo 5

--- Semana04/py05.py
+++ Semana04/py05.py
@@ -1,1 +1,8 @@
 #Aula 05
+
+from multiprocessing.sharedctypes import Value
+
+
+student = {'name': 'Jhon','age':25,'courses':['Math','CompSci']}
+for key, value in student.items():
+    print (key,value)
