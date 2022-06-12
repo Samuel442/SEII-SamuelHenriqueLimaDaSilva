721f8d5c7bf9d94098b4e32bbb263f56f01bf5da
Samuel442_samuelhenrique442@gmail.com <samuelhenrique442@gmail.com>
2022-06-12 11:36:27 -0300

    tarefa pronta

--- Semana05/Async/Async.py
+++ Semana05/Async/Async.py
@@ -1,1 +1,19 @@
-#Aula
+import asyncio
+
+async def procura_numero():
+	print('starting fetching')
+	await asyncio.sleep(2)
+	print('finishing fetching')
+	return{'data':1}
+
+async def print_numbers():
+	for i in range(20):
+		print(i)
+		await asyncio.sleep(1)
+
+async def main():
+	task2 = asyncio.create_task(print_numbers())
+	await procura_numero()
+	await task2
+
+asyncio.run(main())
